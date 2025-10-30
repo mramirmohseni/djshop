80a5d5deda5e27e461031d3f5d401744c7fee0d1
Amir Mohseni <mramirmohseni@gmail.com>
2025-10-30 18:04:03 +0330

    feat(admin): register Category in Django admin

--- src/djshop/apps/catalog/admin.py
+++ src/djshop/apps/catalog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from treebeard.admin import TreeAdmin
+from treebeard.forms import movenodeform_factory
+from .models import Category
+
 
 # Register your models here.
+class CategoryAdmin(TreeAdmin):
+    form = movenodeform_factory(Category)
+
+admin.site.register(Category, CategoryAdmin)
