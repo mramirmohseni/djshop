b779c3516fa299a72dbd93f564b1f3c7283466e2
Amir Mohseni <mramirmohseni@gmail.com>
2025-11-02 17:37:09 +0330

    feat: configure DRF Spectacular for API documentation
    - Add drf_spectacular schema class configuration
    - Configure API documentation settings (Swagger/ReDoc)
    - Add REST framework and API-related apps

--- src/djshop/envs/common.py
+++ src/djshop/envs/common.py
@@ -41,9 +41,7 @@ INSTALLED_APPS = [
     "django_filters",
     "corsheaders",
     "treebeard",
-
     "channels",
-
     "djshop.apps.catalog",
 ]
 
@@ -130,3 +128,16 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    # YOUR SETTINGS
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    # OTHER SETTINGS
+}
