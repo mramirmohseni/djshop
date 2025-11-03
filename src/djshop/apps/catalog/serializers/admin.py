15fc6e7f359529b2c87baee7c92893867c6d3e6d
Amir Mohseni <mramirmohseni@gmail.com>
2025-11-03 15:06:27 +0330

    feat(catalog): extend admin serializers with validation and fields

---
+++ src/djshop/apps/catalog/serializers/admin.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from rest_framework.generics import get_object_or_404
+from apps.catalog.models import Category
+
+
+class CreateCategoryNodeSerializer(serializers.ModelSerializer):
+    parent = serializers.IntegerField()
+
+    def create(self, validated_data):
+        parent = validated_data.pop('parent', None)
+        if parent is None:
+            instance = Category.add_root(**validated_data)
+        else:
+            parent_node = get_object_or_404(Category, pk=parent)
+            instance = parent_node.add_child(**validated_data)
+        return instance
+
+    class Meta:
+        model = Category
+        fields = ('title', 'description', 'is_public', 'slug', 'parent')
