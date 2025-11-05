46499bc2ef033b505670c2b9f5433a2d10c1da37
Amir Mohseni <mramirmohseni@gmail.com>
2025-11-05 14:56:36 +0330

    feat(catalog): implement category tree viewset
    - Filter root categories (depth=1) for list action
    - Use CategoryTreeSerializer for list, CreateCategoryNodeSerializer for CRUD
    - Support hierarchical category operations

--- src/djshop/apps/catalog/views/admin.py
+++ src/djshop/apps/catalog/views/admin.py
@@ -1,9 +1,20 @@
 from rest_framework import viewsets
 
+from djshop.apps.catalog.serializers.admin import CategoryTreeSerializer
 from djshop.apps.catalog.models import Category
 from djshop.apps.catalog.serializers.admin import CreateCategoryNodeSerializer
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
-    queryset = Category.objects.all()
-    serializer_class = CreateCategoryNodeSerializer
+
+    def get_queryset(self):
+        if self.action == "list":
+            return Category.objects.filter(depth=1)
+        else:
+            return Category.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return CategoryTreeSerializer
+        else:
+            return CreateCategoryNodeSerializer
